e051313aa509f7713e7e7e335ebc5ce160c97f31
Alisson Youssf <youssfbr@gmail.com>
2021-08-20 23:34:05 -0300

    EventoController - teste

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController() 
+        {     
+        }
+
+        [HttpGet]
+        public string Get() 
+        {
+           return "value";
+        }
+    }
+}
